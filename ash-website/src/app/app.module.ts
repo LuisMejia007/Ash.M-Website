497f52b376c4604a6ad02beaaafd77682c596571
LuisMejia007 <paranoidandroidz180@gmail.com>
2019-01-20 12:22:00 -0800

    Animation for Read More under Education is almost done. Will place relevant information inside DIV container

--- ash-website/src/app/app.module.ts
+++ ash-website/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { EducationExpComponent } from './education-exp/education-exp.component';
 import { TutoringExpComponentComponent } from './tutoring-exp-component/tutoring-exp-component.component';
@@ -30,7 +30,8 @@ import { QuickEducationInfoComponent } from './quick-education-info/quick-educat
     QuickEducationInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ash-website/src/app/quick-education-info/quick-education-info.component.ts
+++ ash-website/src/app/quick-education-info/quick-education-info.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Education } from '../experience-arrays/education';
 import { educationArray } from '../experience-arrays/educationArray';
-import {SlideDownAnimation} from '../assets/animations/slideDown';
+import { SlideDownAnimation } from 'src/assets/animations/slideDown';
+
 
 @Component({
   selector: 'app-quick-education-info',
   templateUrl: './quick-education-info.component.html',
-  styleUrls: ['./quick-education-info.component.css']
+  styleUrls: ['./quick-education-info.component.css'],
+  animations: [SlideDownAnimation]
 })
 export class QuickEducationInfoComponent implements OnInit {
 
   quickEducationArray: Education[] = educationArray;
-  animationState: 'close';
+  animationState: string = 'close';
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleSlideDown() {
+    this.animationState = this.animationState === 'close' ? 'open' : 'close';
+    console.log('Animation State: ' + this.animationState);
+  }
 }
