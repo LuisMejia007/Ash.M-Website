616955eab3dfd2b9472b01924c07c9e5076a4aa7
LuisMejia007 <paranoidandroidz180@gmail.com>
2019-01-20 23:22:03 -0800

    WIP img slide show functionality in main-bio-page component

--- ash-website/src/app/experience-arrays/belizePhotosArray.ts
+++ ash-website/src/app/experience-arrays/belizePhotosArray.ts
@@ -1,6 +1,6 @@
 export const belizePics: string[] = [
-    '../assets/ashScuba.jpg',
-    '../assets/ashSnakePic.jpg',
+    '../assets/imgs/ashScuba.jpg',
+    '../assets/imgs/ashSnakePic.jpg',
     '../assets/imgs/ashNotesBelize.jpg',
     '../assets/imgs/ashleyCuttingWood.jpg',
     '../assets/imgs/ashWithToad.jpg'
--- ash-website/src/app/main-bio-exp-page/main-bio-exp-page.component.ts
+++ ash-website/src/app/main-bio-exp-page/main-bio-exp-page.component.ts
@@ -7,9 +7,32 @@ import {belizePics} from '../experience-arrays/belizePhotosArray';
 })
 export class MainBioExpPageComponent implements OnInit {
 
+  belizePictures = belizePics;
+  belizePicIndex = 0;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  nextSlide() {
+    const mainPic: HTMLImageElement = document.querySelector('#belizeImg');
+
+    if (this.belizePicIndex >= this.belizePictures.length) {
+
+        this.belizePicIndex = 0;
+        mainPic.src = this.belizePictures[this.belizePicIndex];
+
+    } else if (this.belizePicIndex === this.belizePictures.length - 1) {
+
+      this.belizePicIndex = 0;
+      mainPic.src = this.belizePictures[this.belizePicIndex];
+
+    } else {
+
+      this.belizePicIndex++;
+      mainPic.src = this.belizePictures[this.belizePicIndex];
+    }
+
+  }
 }
