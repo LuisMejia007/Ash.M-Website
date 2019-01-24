f449d0828a440aaceb63eeeb72a8f1e67cc4c72f
LuisMejia007 <paranoidandroidz180@gmail.com>
2019-01-23 23:18:51 -0800

    First attempt in adding animation to side bar navigation

---
+++ ash-website/src/assets/animations/slideFromLeft.ts
@@ -0,0 +1,41 @@
+import {
+    transition,
+    state,
+    trigger,
+    style,
+    animate,
+    group } from '@angular/animations';
+
+export const SlideFromLeftAnimation = [
+    trigger('slideFromLeft', [
+        state('close', style({
+            width: '0px',
+
+        })),
+        state('open', style({
+            width: '50%'
+        })),
+        transition('close => open', [group([
+            animate('1ms ease-in-out', style({
+                width: '10%'
+            })),
+            animate('1000ms ease-in-out', style({
+                width: '30%'
+            })),
+            animate('1200ms ease-in-out', style({
+                width: '50%'
+            }))
+        ])]),
+        transition('open => close', [group([
+            animate('1ms ease-in-out', style({
+                width: '50%'
+            })),
+            animate('1000ms ease-in-out', style({
+                width: '30%'
+            })),
+            animate('1200ms ease-in-out', style({
+                width: '50%'
+            }))
+        ])])
+    ])
+];
