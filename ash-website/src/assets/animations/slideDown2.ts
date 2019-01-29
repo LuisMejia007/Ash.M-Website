3bb2039d6db189b5ec876d84931cf5aaa26e19b0
LuisMejia007 <paranoidandroidz180@gmail.com>
2019-01-28 17:41:32 -0800

    Almost fixed white-spacing issue. Fixed by making display to none inside slideDown2 animation

--- ash-website/src/assets/animations/slideDown2.ts
+++ ash-website/src/assets/animations/slideDown2.ts
@@ -11,26 +11,28 @@ export const SlideDown2Animation =
 trigger('slideDown2', [
     state('up', style({
         maxHeight: '0px',
-        opacity: '0.0'
+        opacity: '0.0',
+        display: 'none'
     })),
     state('down', style({
         maxHeight: '600px',
         opacity: '1.0',
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        display: 'block'
     })),
 
     transition('up=>down', [group([
         animate('1000ms ease-in', style({
             maxHeight: '600px',
             opacity: '1.0',
-            backgroundColor: 'black'
+            display: 'block'
         }))
     ])]),
     transition('down=>up', [group([
         animate('1000ms ease-out', style({
             maxHeight: '0px',
             opacity: '0.5',
-            backgroundColor: 'black'
+            display: 'none'
         }))
     ])])
 ]);
