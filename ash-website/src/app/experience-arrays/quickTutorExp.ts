7703f18a21645e59662fbcbebfc360648f382232
LuisMejia007 <paranoidandroidz180@gmail.com>
2019-02-04 12:44:09 -0800

    Complete re-work of tutoring experience component inside the homepage per request. Will now start working on mobile-responsiveness

--- ash-website/src/app/experience-arrays/quickTutorExp.ts
+++ ash-website/src/app/experience-arrays/quickTutorExp.ts
@@ -2,5 +2,8 @@ import {QuickExp} from './quickExperience';
 
 
 export const quickTutorExp: QuickExp[] = [
-    {summary: 'I love researching and learning new things, but I also love teaching what I learned and giving back to the global community. Whether its helping the current generation or the next, preparing others to succeed is another way I hope to make the world a better place. ', photos: ['../assets/imgs/ashRussiaTeaching.jpg', '../assets/imgs/ashTutorWhiteBoard.jpg', '../assets/imgs/ashTeaching.jpg']}
+    {
+        summary: 'I love researching and learning new things, but I also love teaching what I learned and giving back to the global community. Whether its helping the current generation or the next, preparing others to succeed is another way I hope to make the world a better place. ', 
+        photos: ['../assets/imgs/ashRussiaTeaching.jpg']
+    }
 ];
