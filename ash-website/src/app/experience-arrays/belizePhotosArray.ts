528bdffa827cada99874032bf3d54f8bc824383a
LuisMejia007 <paranoidandroidz180@gmail.com>
2019-02-08 20:02:16 -0800

    Had to fix some picture descriptions and page mobile-responsiveness. Page is completed for the most part

--- ash-website/src/app/experience-arrays/belizePhotosArray.ts
+++ ash-website/src/app/experience-arrays/belizePhotosArray.ts
@@ -1,9 +1,9 @@
 import { Picture } from './picture';
 
 export const belizePics: Picture[] = [
-    {url: '../assets/imgs/ashScuba.jpg', description: 'Taking field notes in Belize'},
-    {url: '../assets/imgs/ashSnakePic.jpg', description: 'Touching a snake'},
-    {url: '../assets/imgs/ashNotesBelize.jpg', description: 'Writing down results of the day\'s research.'},
+    {url: '../assets/imgs/ashScuba.jpg', description: 'Taking field notes in Belize\'s coral reef.'},
+    {url: '../assets/imgs/ashSnakePic.jpg', description: 'We we\'re planning to use this snake for our studies that day, however it was too skittish.'},
+    {url: '../assets/imgs/ashNotesBelize.jpg', description: 'Started my day by taking notes of different plant species we planned to study that day.'},
     {url: '../assets/imgs/ashleyCuttingWood.jpg', description: 'Taking samples from tree in order to measure it\'s health. This gave us insight to the surrounding ecosystem\'s health. '},
     {url: '../assets/imgs/ashWithToad.jpg', description: 'Here I am with a toad that we used that day to study.'}
 ];
